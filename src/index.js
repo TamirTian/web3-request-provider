3aa90e721610d6d83d28c777c7cc9b93090e76c0
Tamir <tamir@inject.top>
2019-11-16 18:25:55 +0800

    keepalive

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-const request = require('request')
+const axios = require('axios')
+const http = require('http')
+const https = require('https')
+const httpAgent = new http.Agent({ keepAlive: true })
+const httpsAgent = new https.Agent({ keepAlive: true })
 
 var HttpProvider = function HttpProvider (host, options) {
 	options = options || {}
 
 	this.host = host || 'http://localhost:8545'
-	this.request = request.defaults({
+	this.axios = axios.create({
+		httpAgent,
+		httpsAgent,
+		baseURL: this.host,
 		headers: { 'content-type': 'application/json', ...options.headers },
 		timeout: options.timeout || 120 * 1000
 	})
 }
 
 HttpProvider.prototype.send = function (payload, callback) {
-	this.request.post({ url: this.host, json: payload }, function (err, httpResponse, body) {
-		callback(err, body)
-	})
+	this.axios.post('/', payload)
+		.then(res => callback(null, res.data))
+		.catch(callback)
 }
 
 HttpProvider.prototype.disconnect = function () {
